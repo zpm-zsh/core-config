90b236fbff8e4a7c48fd39a666a6a2c4dbbc1c0b
Grigorii Horos <horosgrisa@gmail.com>
2019-04-18 17:09:19 +0300

    Initial commit

---
+++ core-config.plugin.zsh
@@ -0,0 +1,8 @@
+source ${${(%):-%x}:a:h}/completions.zsh
+source ${${(%):-%x}:a:h}/correct.zsh    
+source ${${(%):-%x}:a:h}/history.zsh    
+source ${${(%):-%x}:a:h}/keyboard.zsh   
+source ${${(%):-%x}:a:h}/modules.zsh    
+source ${${(%):-%x}:a:h}/cd.zsh         
+source ${${(%):-%x}:a:h}/path-fix.zsh   
+
