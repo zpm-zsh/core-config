f21573452f7074e124d08da666ab3f7a2f7484b0
Grigorii Horos <horosgrisa@gmail.com>
2019-07-21 16:13:13 +0300

    Fix vars

--- vars.zsh
+++ vars.zsh
@@ -3,17 +3,7 @@
 : ${EDITOR:="vim"}
 : ${VISUAL:="vim"}
 : ${PAGER:="less"}
-: ${HISTSIZE:=9999}
-: ${SAVEHIST:=9999}
-: ${LISTMAX:=9999}
 
 export EDITOR
 export VISUAL
 export PAGER
-export HISTSIZE
-export SAVEHIST
-export LISTMAX
-
-WORDCHARS='*?_[]~=&;!#$%^(){}<>:.'
-HISTFILE="$HOME/.zsh_history"
-ZSH_COMPDUMP="$HOME/.zcompdump"
