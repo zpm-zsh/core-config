a1e355f4782052c2dbd1844a730020973cc4d05d
Grigorii Horos <horosgrisa@gmail.com>
2019-07-21 14:06:54 +0300

    Add vars

--- core-config.plugin.zsh
+++ core-config.plugin.zsh
@@ -4,11 +4,12 @@ if command -v zpm >/dev/null; then
   zpm zpm-zsh/helpers
 fi
 
+source ${${(%):-%x}:a:h}/vars.zsh
 source ${${(%):-%x}:a:h}/completions.zsh
-source ${${(%):-%x}:a:h}/correct.zsh    
-source ${${(%):-%x}:a:h}/history.zsh    
-source ${${(%):-%x}:a:h}/keyboard.zsh   
-source ${${(%):-%x}:a:h}/modules.zsh    
-source ${${(%):-%x}:a:h}/cd.zsh         
-source ${${(%):-%x}:a:h}/path-fix.zsh   
+source ${${(%):-%x}:a:h}/correct.zsh
+source ${${(%):-%x}:a:h}/history.zsh
+source ${${(%):-%x}:a:h}/keyboard.zsh
+source ${${(%):-%x}:a:h}/modules.zsh
+source ${${(%):-%x}:a:h}/cd.zsh
+source ${${(%):-%x}:a:h}/path-fix.zsh
 
