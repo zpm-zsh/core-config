b961e45e8f12c20b509f716a40c26e2620db998f
Grigorii Horos <horosgrisa@gmail.com>
2019-07-27 23:06:55 +0300

    Add underline for first letter

--- correct.zsh
+++ correct.zsh
@@ -2,7 +2,13 @@
 
 setopt correct
 if [[ "$CLICOLOR" = 1 ]]; then
-  SPROMPT='$c[cyan] Change "$c[red]%R$c[cyan]" to "$c[green]%r$c[cyan]"? ($c[green]Yes$c[cyan], $c[red]No$c[cyan], $c[red]Abort$c[cyan], $c[yellow]Edit$c[cyan]) $c[blue]> $c_reset'
+  SPROMPT='$c[cyan] Change "$c[red]%R$c[cyan]" '
+  SPROMPT+='to "$c[green]%r$c[cyan]"? '
+  SPROMPT+='($c[green]${c_underline}Y$c_reset$c[green]es$c[cyan], '
+  SPROMPT+='$c[red]${c_underline}N${c_reset}${c[red]}o$c[cyan], '
+  SPROMPT+='$c[red]${c_underline}A${c_reset}${c[red]}bort$c[cyan], '
+  SPROMPT+='$c[yellow]${c_underline}E${c_reset}${c[yellow]}dit$c[cyan]) '
+  SPROMPT+='$c[blue]> $c_reset'
 else
   SPROMPT=' Change '%R' to '%r'? (Yes, No, Abort, Edit) > '
 fi
