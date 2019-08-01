060beb019488477c2d31dda23ef35410dfd994e0
Grigorii Horos <horosgrisa@gmail.com>
2019-08-01 10:42:45 +0300

    Fix cd to user home

--- cd.zsh
+++ cd.zsh
@@ -5,8 +5,6 @@ setopt autopushd            # Push the old directory onto the stack on cd.
 setopt pushdignoredups      # Do not store duplicates in the stack.
 setopt pushdsilent          # Do not print the directory stack after pushd or popd.
 setopt pushdtohome          # Push to home directory when no argument is given.
-setopt cdablevars           # Change directory to a path stored in a variable.
-
 
 alias d='dirs -v'
 for index ({0..9}) alias "$index"="cd +${index}"; unset index
