de21142f286d923c87cbf78d7bee8a2f6583a2dd
Grigorii Horos <horosgrisa@gmail.com>
2019-08-01 10:31:27 +0300

    Add quote-magic

--- modules.zsh
+++ modules.zsh
@@ -1,5 +1,20 @@
 #!/usr/bin/env zsh
 
+autoload -Uz is-at-least
+if [[ ${ZSH_VERSION} != 5.1.1 && ${TERM} != "dumb" ]]; then
+  if is-at-least 5.2; then
+    autoload -Uz bracketed-paste-url-magic
+    zle -N bracketed-paste bracketed-paste-url-magic
+  else
+    if is-at-least 5.1; then
+      autoload -Uz bracketed-paste-magic
+      zle -N bracketed-paste bracketed-paste-magic
+    fi
+  fi
+  autoload -Uz url-quote-magic
+  zle -N self-insert url-quote-magic
+fi
+
 setopt braceccl
 setopt interactive_comments
 setopt nohup
@@ -11,3 +26,7 @@ setopt multios              # Write to multiple descriptors.
 setopt extendedglob        # Use extended globbing syntax.
 unsetopt clobber            # Do not overwrite existing files with > and >>.
 # Use >! and >>! to bypass.
+setopt COMBINING_CHARS
+
+
+
