fffa46323ecf3f8c1ebf5208fe0195294cbd3fbf
Grigorii Horos <horosgrisa@gmail.com>
2019-11-16 00:17:48 +0200

    Fix TMPDIR

--- core-config.plugin.zsh
+++ core-config.plugin.zsh
@@ -75,20 +75,7 @@ else
 fi
 
 zstyle ":completion::complete:*" use-cache 1
-zstyle ":completion::complete:*" cache-path "$HOME/.cache/zsh"
-
-zstyle ':completion:*:*:*:users' ignored-patterns \
-  adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
-  dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
-  hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
-  mailman mailnull mldonkey mysql nagios \
-  named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-  operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-  rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs \
-  colord dnsmasq geoclue git gluster haveged lightdm polkitd \
-  rtkit sddm usbmux uuidd \
-  'deepin*' 'systemd-*' '_*'
-
+zstyle ":completion::complete:*" cache-path "${TMPDIR:-/tmp}/zsh-${UID}/cache"
 
 
 zmodload zsh/complist
