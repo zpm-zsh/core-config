65fc8c0aced4768c3a540291f721e7618613bae4
Grigorii Horos <horosgrisa@gmail.com>
2020-04-16 16:46:20 +0300

    Optimize

--- core-config.plugin.zsh
+++ core-config.plugin.zsh
@@ -1,24 +1,12 @@
 #!/usr/bin/env zsh
 WORDCHARS='*?_[]~=&;!#$%^(){}<>:.-'
-HISTFILE=~/.zsh_history
-HISTSIZE=1000
 SAVEHIST=1000
-
-export EDITOR='vim'
-export VISUAL='vim'
-export PAGER='less'
 
 setopt append_history
 setopt autocd
 setopt braceccl
 setopt combining_chars
 setopt correct
-setopt hist_expire_dups_first
-setopt hist_find_no_dups
-setopt hist_ignore_dups
-setopt hist_ignore_space
-setopt hist_reduce_blanks
-setopt hist_verify
 setopt inc_append_history
 setopt interactive_comments
 setopt multios
@@ -29,7 +17,6 @@ setopt nohup
 setopt numericglobsort
 
 unsetopt clobber
-unsetopt extended_history
 unsetopt bg_nice
 
 zstyle ':completion:*:processes' command 'NOCOLORS=1 ps -U $USER|sed "/ps/d"'
@@ -38,12 +25,11 @@ zstyle ':completion:*:processes-names' command 'NOCOLORS=1 ps xho command|sed "s
 zstyle ':completion:*:processes' sort false
 
 zstyle ':completion:*' completer _expand _complete _correct _approximate
-zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+zstyle ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
 
 zstyle '*' single-ignored show
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-zstyle ':completion:*' special-dirs true
 
 zstyle ':completion:*:messages' format '%d'
 
@@ -52,13 +38,11 @@ zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*:*:zcompile:*' ignored-patterns '(*~|*.zwc)'
 
-zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
-
 zstyle ':completion:*:warnings' format "%{${c[red]}${c[bold]}%}No matches for:%{${c[yellow]}${c[bold]}%} %d"
 
 if [[ "$CLICOLOR" != '0' ]]; then
   zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=36=31'
-  zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==36=36}:${(s.:.)LS_COLORS}")'
+  zstyle ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==36=36}:${(s.:.)LS_COLORS}")'
   zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} 'ma=7;33'
 fi
 
@@ -106,6 +90,7 @@ bindkey '^X^E' edit-command-line
 
 bindkey -M menuselect ' ' accept-and-menu-complete
 
+# CD
 alias tmp='cd $(mktemp -d)'
 alias cdo='cd -'
 alias -- -='cd -'
@@ -115,6 +100,24 @@ alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
 
+# EDITOR 
+export EDITOR='vim'
+export VISUAL='vim'
+export PAGER='less'
 
+# HISTORY
+HISTFILE="${HOME}/.zsh_history"
+HISTSIZE=5000
+
+setopt hist_expire_dups_first
+setopt hist_find_no_dups
+setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt hist_verify
+
+unsetopt extended_history
+
+# PATH
 appendpath ~/.bin
 appendpath ~/.local/bin
